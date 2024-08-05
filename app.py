76ea732c2456c1c6af4ced888f51d79773568d4b
Muhammad Ahmed <muhammad.ahmed2315@gmail.com>
2024-08-05 21:19:34 +0100

    Added user authentication functionality and a 90% working login and register pages

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_migrate import Migrate
+from extensions import db, login_manager
+
+import logging
+
+log = logging.getLogger("werkzeug")
+log.disabled = True
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecretkey"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite"
+
+db.init_app(app)
+Migrate(app, db)
+
+login_manager.init_app(app)
+login_manager.login_view = "user_authentication.login"
+
+from login.app import user_authentication
+
+app.register_blueprint(user_authentication)
+
+# Create tables if they don't exist, within the application context
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ login/app.py
@@ -0,0 +1,78 @@
+from flask import render_template, redirect, request, url_for, Blueprint, flash
+from flask_login import login_user, login_required, logout_user
+from models import User
+from .forms import LoginForm, RegisterForm
+from extensions import db
+from validate_email_address import validate_email
+
+user_authentication = Blueprint("user_authentication", __name__)
+
+
+@user_authentication.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@user_authentication.route("/login", methods=["get", "post"])
+def login():
+    loginForm = LoginForm()
+
+    if loginForm.is_submitted() and loginForm.validate():
+        # if email has incorrect format
+        if not validate_email(loginForm.email.data):
+            loginForm.email.errors.append("Email is invalid")
+            return render_template("login.html", loginForm=loginForm)
+
+        # if email not registered or password incorrect
+        user = User.query.filter_by(email=loginForm.email.data).first()
+        if not user or (user and not user.check_password(loginForm.password.data)):
+            loginForm.password.errors.append("Incorrect email or password")
+            return render_template("login.html", loginForm=loginForm)
+
+        # else login user
+        login_user(user)
+
+        next = request.args.get("next")
+
+        if next == None or next[0] != "/":
+            next = url_for("user_authentication.home")
+
+            return redirect(next)
+
+    return render_template("login.html", loginForm=loginForm)
+
+
+@user_authentication.route("/register", methods=["get", "post"])
+def register():
+    registerForm = RegisterForm()
+
+    if registerForm.is_submitted() and registerForm.validate():
+        # Check if valid email
+        if not validate_email(registerForm.email.data):
+            registerForm.email.errors.append("Email is invalid")
+            return render_template("register.html", registerForm=registerForm)
+
+        # Check if email already registered
+        user = User.query.filter_by(email=registerForm.email.data).first()
+        if user:
+            registerForm.email.errors.append(
+                "You have already registered with this email address."
+            )
+            return render_template("register.html", registerForm=registerForm)
+
+        # Check if passwords match
+        if registerForm.password.data != registerForm.pass_confirm.data:
+            registerForm.password.errors.append("Password and confirmation don't match")
+            registerForm.pass_confirm.errors.append(
+                "Password and confirmation don't match"
+            )
+            return render_template("register.html", registerForm=registerForm)
+
+        else:
+            user = User(registerForm.email.data, registerForm.password.data)
+            db.session.add(user)
+            db.session.commit()
+            # login_user(user)
+            return redirect(url_for("user_authentication.home"))
+
+    return render_template("register.html", registerForm=registerForm)
